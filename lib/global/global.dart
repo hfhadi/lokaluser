0014a1a611b39fbb39a4b8e4748a33340e21e513
Salam <salam73@gmail.com>
2022-10-20 13:57:27 +0300

    استخدام حرف g للتعبير عن global variables

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -3,17 +3,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../models/direction_details_info.dart';
 import '../models/user_model.dart';
 
-final FirebaseAuth fAuth = FirebaseAuth.instance;
-User? currentFirebaseUser;
-UserModel? userModelCurrentInfo;
-List sortedDriverList = []; //online-active drivers Information List
-DirectionDetailsInfo? tripDirectionDetailsInfo;
-String? chosenDriverId = "";
-String cloudMessagingServerToken =
+final FirebaseAuth gFirebaseAuth = FirebaseAuth.instance;
+User? gCurrentFirebaseUser;
+UserModel? gUserModelCurrentInfo;
+List gSortedDriverList = []; //online-active drivers Information List
+DirectionDetailsInfo? gTripDirectionDetailsInfo;
+String? gChosenDriverId = "";
+String gCloudMessagingServerToken =
     "key=AAAAOING-Is:APA91bHbsB6xKiIf1u18UQaPU_M_e4ZKm2XRl0h0QpIkXzEWbKH6OJfzr4-k5lTlpfuvU5Lzdxu0TtxgZnEC7tpFlYtLHtqejzpJ6B1ecpVN0bO4a8Opx6-Zkt_TDjxfte1dJbIuxEav";
-String userDropOffAddress = "";
-String driverCarDetails = "";
-String driverName = "";
-String driverPhone = "";
-double countRatingStars = 0.0;
+String gUserDropOffAddress = "";
+String gDriverCarDetails = "";
+String gDriverName = "";
+String gDriverPhone = "";
+double gCountRatingStars = 0.0;
 String titleStarsRating = "";
